9e242fa63f8e1d901ca80370df63563de8d29c11
caroline parra gomes <Cargomes@server01.foursys.local>
2021-12-11 23:02:07 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Crie um programa com uma ou duas variáveis atribuindo valores iniciais
+        // e exiba a o resultado de sua exponenciação;
+
+        double valor1;
+        double valor2;
+
+
+        valor1 = 10;
+        valor2 = 25;
+
+        System.out.println(valor1);
+        double resultado1 =  Math.pow(valor1, 2);
+        System.out.println(resultado1);
+
+        System.out.println(valor2);
+        double resultado2 =  Math.pow(valor2, 2);
+        System.out.println(resultado2);
+
+
+
+    }
+}
